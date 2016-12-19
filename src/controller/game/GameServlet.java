3800bf25f73cfc83bc584c543496d82eb34a9cd1
indeep-xyz <indeep.xyz@gmail.com>
2016-12-19 10:38:10 +0900

    Remove import statements never used

--- src/controller/game/GameServlet.java
+++ src/controller/game/GameServlet.java
@@ -8,10 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import controller.tool.ParameterAgent;
-import model.field.FieldCreator;
 
 /**
  * ゲーム画面の表示をおこなうサーブレット。
--- src/controller/game/data/FieldDataServlet.java
+++ src/controller/game/data/FieldDataServlet.java
@@ -3,18 +3,14 @@ package controller.game.data;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import controller.game.GameMaster;
 import controller.tool.ParameterAgent;
-import model.field.Field;
-import model.field.FieldCreator;
 
 /**
  * ゲームのフィールド情報の出力を行うサーブレット。
